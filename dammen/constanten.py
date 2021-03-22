bf02c86c5f9b599e5bd3c8552ed8027d57c42aaf
JerryvS <77832782+JerryvS@users.noreply.github.com>
2021-03-22 10:42:21 +0100

    Update constanten.py

--- dammen/constanten.py
+++ dammen/constanten.py
@@ -1,6 +1,8 @@
 import pygame
 
 kroon = pygame.transform.scale(pygame.image.load('dammen/plaatje/kroon.png'), (40, 30))
+zwart_winnaar = pygame.transform.scale(pygame.image.load('dammen/plaatje/zwartwin.png'), (214, 172))
+wit_winnaar = pygame.transform.scale(pygame.image.load('dammen/plaatje/witwint.png'), (217, 174))
 
 framerate = 10
 kolommen = 10
